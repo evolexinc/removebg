4b26924543393dbc2edc4dfba84a98eb40025d75
Evolex <100163406+evolexinc@users.noreply.github.com>
2024-07-28 02:11:36 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#pip install rembg
+#Remove Image Background using Python
+from rembg import remove
+from PIL import Image
+input_path = 'cl.jpg’
+output_path = 'output.png’
+input = Image.open(input_path)
+output = remove (input)
+output.save(output_path)
